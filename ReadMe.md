6f56582239f9ef24f58b51f328582a4f6251d9a4
ImCheeini <59221034+Nicat-dcw@users.noreply.github.com>
2022-06-29 19:45:51 +0400

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,44 @@
+# Uptimes - Powerfull Uptime Package for Websites And More....
+
+## Setup;
+```js
+npm install uptimes@latest
+```
+Notice: This Package Is BETA!
+
+
+## Start;
+```js
+const Client = require("../index")
+const Uptime = new Client()
+Uptime.settings({
+    reqTimeout: 12000 //Request Timeout Example: 1 Seconds = 1000, 10 seconds = 10000
+})
+```
+
+## Start UPTIME a Website;
+```js
+Uptime.uptime("https://api.nicat-dcw.xyz")
+```
+
+## Uptime Yourself!! (NEW!)
+```js
+Uptime.uptimeMe()
+```
+
+**🧤 | List of Commands**
+
+Name | Description | Type  | Timeout | Default Timeout
+------------ | ------------ | ------------- | ------------- | ------------- |
+uptime     | Uptimes a Link | URL | Settings With | 15000 (15 seconds)
+uptimeMe      | Uptimes Yourself | creates Server | 150000 | 25000
+
+** 📜 List of Settings**
+Name | Description | Type  |
+------------ | ------------ | ------------- |
+reqTimeout | Request Timeout | NUMBER
+
+Developed By Nicat.Dcw ❤️
+
+**Documentation**: https://api.nicat-dcw.xyz (Cooming Soon....)
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const request = require("axios")
+const db = require("inflames.db")
+module.exports = class uptimess{
+ setUptime(){
+  const fetc = db.get(`uptimes`)
+  setInterval(function(){
+  request.get("https://nicat-dcw.xyz")
+  .then(function (response) {
+    // handle success
+    console.log(response && response.size);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  },15000)
+}
+settings(url, config){
+    if(!url) throw new TypeError("Please Validate A Config.")
+    if(url === "reqTimeout"){
+        if(!config.content) throw new TypeError("Please valid A Number with milseconds format! Example: 1 seconds = 1000 , 10 seconds = 10000")
+        db.set(`uptimeTimeout`, config.content)
+    }
+}
+
+     uptime(url){
+         const Timeout = db.fetch(`uptimeTimeout`)
+      if(!url.startsWith === "https://" && !url.startsWith === "http://") throw new TypeError("Link starts must be https:// or http://")
+      db.push(`uptimes`, {url: url})
+      const fetc = db.get(`uptimes`)
+      let axios = request.create({
+  baseURL: url,
+  timeout: Timeout,
+  withCredentials: true
+});
+  setInterval(function(){
+  axios.get(url)
+  .then(function (response) {
+    // handle success
+    console.log(response && response.size);
+  })
+  .catch(function (error) {
+    // handle error
+    throw new TypeError(error)
+  })
+  .then(function () {
+    // always executed
+  });
+  },Timeout || 15000)
+      return `Link Added! Uptime Starts will 15 seconds.`
+    }
+     uptimeMe(){
+      db.set(`uptime.me`, true)
+     setInterval(function(){
+  require('http').createServer((req, res) => res.end('OK')).listen(3000)
+},15000 || 25000)
+      return `Successfully!`
+    }
+  
+}
